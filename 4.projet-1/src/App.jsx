9401bbd62df6f18dfed0f3791c6e7de707ea174c
yves40 <yves.toubhans@free.fr>
2024-12-22 11:42:05 +0100

    Création de la page d'accueil

--- 4.projet-1/src/App.jsx
+++ 4.projet-1/src/App.jsx
@@ -1,12 +1,19 @@
 
-import './App.css'
+// import './App.css'
 
 function App() {
 
   return (
-    <>
-    <h1 className='bg-red-400 text-3xl'>The amazing tasks application</h1>
-    </>
+    <div className="h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        <h1 className='text-3xl text-slate-100 mb-4'>La To-do liste</h1>
+        <form className='mb-10'>
+          <label htmlFor="todo-item" className='text-slate-50'>Ajouter une tâche</label>
+          <input id='todo-item' type="text" className='mt-1 block w-full rounded'/>
+          <button className='mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]'>Ajouter</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- 4.projet-1/src/main.jsx
+++ 4.projet-1/src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-  </StrictMode>,
 )

e86c287b0b83120ceef2a92c5bf756298a0e0ba0
yves40 <yves.toubhans@free.fr>
2024-12-24 11:30:02 +0100

    Step 1 : Display and delete tasks

--- 4.projet-1/src/App.jsx
+++ 4.projet-1/src/App.jsx
@@ -1,7 +1,25 @@
 
 // import './App.css'
 
+import { useState } from "react"
+import { nanoid } from "nanoid";
+import ListItem from './components/ListItem'
+
 function App() {
+
+
+  const [todoList, setTodoList] = useState([ 
+    {id: nanoid(8), content: 'item 1' },
+    {id: nanoid(8), content: 'item 2' },
+    {id: nanoid(8), content: 'item 3' },
+    {id: nanoid(8), content: 'item 4' },
+  ]);
+
+  console.log(todoList);
+  
+  function deleteTodo(id) {
+    setTodoList(todoList.filter(task =>  id != task.id));
+  }
 
   return (
     <div className="h-screen bg-slate-900">
@@ -11,6 +29,11 @@ function App() {
           <label htmlFor="todo-item" className='text-slate-50'>Ajouter une tâche</label>
           <input id='todo-item' type="text" className='mt-1 block w-full rounded'/>
           <button className='mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]'>Ajouter</button>
+          <ul>
+            {todoList.map( 
+                item => <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo}/>
+            )}
+          </ul>
         </form>
       </div>
     </div>

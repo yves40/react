a3acbdf0eae52aebfcc10a78e66f454ea4ecd392
yves40 <yves.toubhans@free.fr>
2025-01-11 18:54:50 +0100

    tailwind install : step 3

---
+++ 6.Créer-des-composants-courants/src/App.jsx
@@ -0,0 +1,12 @@
+import ModalButton from "./lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButton"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-slate-700 pt-10">
+      <ModalButton></ModalButton>
+    </div>
+  )
+}
+
+export default App

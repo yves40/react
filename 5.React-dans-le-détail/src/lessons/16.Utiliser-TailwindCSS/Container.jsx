2574d11ea046b1b52ab36cdc5d0f2e580f6cc37c
yves40 <yves.toubhans@free.fr>
2025-01-11 18:55:20 +0100

    Previuos tailwind presentation

--- 5.React-dans-le-détail/src/lessons/16.Utiliser-TailwindCSS/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/16.Utiliser-TailwindCSS/Container.jsx
@@ -1,7 +1,10 @@
+/*
+  Install and configure tailwind 1st
+*/
 export default function Container() {
   return (
     <div>
-      <h1>Utiliser Tailwind</h1>
+      <h1 className="bg-red-900 mt-4 p-7">Utiliser Tailwind</h1>
     </div>
   )
 }

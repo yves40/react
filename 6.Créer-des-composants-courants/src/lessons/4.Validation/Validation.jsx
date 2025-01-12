773a097cba5528a0be9b1c05d2fe9ba6056ac020
yves40 <yves.toubhans@free.fr>
2025-01-12 12:45:16 +0100

    Button color

--- 6.Créer-des-composants-courants/src/lessons/4.Validation/Validation.jsx
+++ 6.Créer-des-composants-courants/src/lessons/4.Validation/Validation.jsx
@@ -80,7 +80,8 @@ export default function Validation() {
                 setInputStates={setInputStates}
                 showvalidation={showvalidation}>
             </Confirmation>
-            <button className="mt-10 bg-slate-100 px-4 py-2 min-w-[125px] rounded">Valider</button>
+            <button className="mt-10 bg-green-800 px-4 py-2 min-w-[125px] 
+                            rounded text-slate-100">Valider</button>
         </form>
     )
 }

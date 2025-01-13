facf4c1a20aa543e85f8f7d0a8745c3073646828
yves40 <yves.toubhans@free.fr>
2025-01-13 12:31:22 +0100

    Redux phase 1

---
+++ 9.React-Redux/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './pages/Counter'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-700 px-4">
+      <div className="mx-auto max-w-3xl pt-10 pb-20">
+        <Counter></Counter>
+      </div>
+    </div>
+  )
+}
+
+export default App

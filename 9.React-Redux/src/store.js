56e41f502633745c87980988847190c3aa9a6a66
yves40 <yves.toubhans@free.fr>
2025-01-13 12:38:29 +0100

    Fix import error, removed parenthesis

--- 9.React-Redux/src/store.js
+++ 9.React-Redux/src/store.js
@@ -1,8 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
-import { counter } from './reduxfeatures/counter'
+import counter from './reduxfeatures/counter'
 
 export const store = configureStore({
     reducer: {
-        counter : counter.reducer
+        counter
     }
 })

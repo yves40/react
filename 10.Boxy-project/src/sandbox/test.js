060271f111fa7b80b681ece2d64ab3dbbd93c527
yves40 <yves.toubhans@free.fr>
2025-01-16 11:01:32 +0100

    Checl some objct syntax

---
+++ 10.Boxy-project/src/sandbox/test.js
@@ -0,0 +1,15 @@
+
+const initialState = 
+    {
+        id: 12,
+        active: true,
+        inset: false,
+    }
+
+
+
+
+console.log(initialState.active);
+
+console.log(initialState["active"]);
+

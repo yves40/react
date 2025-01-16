11396d23420fe99dda09078485effeff4aae1611
yves40 <yves.toubhans@free.fr>
2025-01-16 17:48:04 +0100

    Boxy gen: phase 12 : button animation

--- 10.Boxy-project/src/layouts/Visualization/Modal/ModalResult.jsx
+++ 10.Boxy-project/src/layouts/Visualization/Modal/ModalResult.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { useEffect } from 'react'
 import getBoxShadowValue from '../../../utils/getBoxShadowValue'
 import { useSelector } from 'react-redux'
 
 export default function ModalResult({closeModal}) {
 
   const shadowValues = useSelector(state => state.shadows)
+
+  useEffect(() => {
+    document.body.style.overflowY = "hidden"
+
+    return () =>  document.body.style.overflowY = "auto"
+
+  }, [])
+
+  let animationrunning = false
+  function handleAnimation(e) {
+    animationrunning = true
+    e.target.textContent = 'Copied!'
+    setTimeout(() => {
+        animationrunning = false
+        e.target.textContent = 'Copy'
+    }, 1500)
+
+    navigator.clipboard.writeText(`box-shadow: ${getBoxShadowValue(shadowValues)}`)
+  }
 
   return (
     <div className='fixed z-10 inset-0 flex items-center justify-center bg-gray-600/75'
@@ -15,7 +34,8 @@ export default function ModalResult({closeModal}) {
       >
         <div className='flex items-end mb-5'>
           <p className='font-semibold mr-5'>Here is the code 😡🥵</p>
-          <button className='ml-auto mr-2 text-sm bg-blue-600 text-white hover:bg-blue-700 py-1 px-3 rounded'>
+          <button className='ml-auto mr-2 text-sm bg-blue-600 text-white hover:bg-blue-700 py-1 px-3 rounded'
+              onClick={handleAnimation}>
             Copy
           </button>
           <button className='text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded'

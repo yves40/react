1f39c047bcd10dc5d00dbe3649226f35fc02fc9c
yves40 <yves.toubhans@free.fr>
2025-01-16 17:57:34 +0100

    End of react training

--- 10.Boxy-project/src/components/Footer.jsx
+++ 10.Boxy-project/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
 
-  const versiondate = "1.10 Jan 14 2025"
+  const versiondate = "1.11 Jan 16 2025"
 
   return (
     <footer className='p-2 border-t-2 border-gray-200'>
